46299920ac842a222873122bc9f106896fc78679
yurimoura25 <y2moura@hotmail.com>
2024-11-26 14:59:32 -0300

    First commit

---
+++ src/main/java/com/yuri/ubots_test/UbotsTestApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.ubots_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbotsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbotsTestApplication.class, args);
+	}
+
+}
